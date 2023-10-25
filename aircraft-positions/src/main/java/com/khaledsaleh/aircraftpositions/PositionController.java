ff11d8e53fb6543fb9fdb204eedcba09368169bf
Khaled Saleh <th.be03@gmail.com>
2023-10-25 15:58:06 +0200

    first commit

---
+++ aircraft-positions/src/main/java/com/khaledsaleh/aircraftpositions/PositionController.java
@@ -0,0 +1,20 @@
+package com.khaledsaleh.aircraftpositions;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RequiredArgsConstructor
+@RestController
+public class PositionController {
+    @NonNull
+    private final PositionRetriever retriever;
+
+    @GetMapping("/aircraft")
+    public Iterable<Aircraft> getCurrentAircraftPositions() {
+        return retriever.retrieveAircraftPositions();
+    }
+}
---
+++ aircraft-positions/src/test/java/com/khaledsaleh/aircraftpositions/PositionControllerTest.java
@@ -0,0 +1,56 @@
+package com.khaledsaleh.aircraftpositions;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.Instant;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@WebFluxTest(controllers = {PositionController.class})
+class PositionControllerTest {
+    @MockBean
+    private PositionRetriever retriever;
+
+    private Aircraft ac1, ac2;
+    @BeforeEach
+    void setUp() {
+        ac1 = new Aircraft(1L, "SAL001", "sqwk", "N12345", "SAL001",
+                "STL-SFO", "LJ", "ct",
+                30000, 280, 440, 0, 0,
+                39.2979849, -94.71921, 0D, 0D, 0D,
+                true, false,
+                Instant.now(), Instant.now(), Instant.now());
+        ac2 = new Aircraft(2L, "SAL002", "sqwk", "N54321", "SAL002",
+                "SFO-STL", "LJ", "ct",
+                40000, 65, 440, 0, 0,
+                39.8560963, -104.6759263, 0D, 0D, 0D,
+                true, false,
+                Instant.now(), Instant.now(), Instant.now());
+
+        Mockito.when(retriever.retrieveAircraftPositions()).thenReturn(List.of(ac1, ac2));
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getCurrentAircraftPositions(@Autowired WebTestClient client) {
+        final Iterable<Aircraft> acPositions = client.get()
+                .uri("/aircraft")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Aircraft.class)
+                .returnResult()
+                .getResponseBody();
+        assertEquals(List.of(ac1, ac2), acPositions);
+    }
+}
